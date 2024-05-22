669b55e8918a9429a67d3c68fe3d1b4e64739d10
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-05-22 19:42:28 +0530

    Update Init_Array.java

--- Init_Array.java
+++ Init_Array.java
@@ -1,1 +1,11 @@
+//One element at time
+for (int i = 0; i < array.length; i++) {
+    array[i] = i + 2;
+}
 
+//Multiple element at time
+for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 5; j++) {
+        array[i][j] = j + 1;
+    }
+}
