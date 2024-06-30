a06add34f31b50e6a4917085b0dff194b85aa885
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-06-30 21:24:15 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,20 @@
+public class {
+  public static void main(){
+
+
+    
+    for(int i=0; i<100; i++){
+     System.out.println("Jay shree ram");                     // for loop
+    }
+
+     
+     int a =80;
+	   while(a<100) {
+		   a++;
+		   if(a==95) continue;                                             // continue skip the numbers
+		   System.out.println(a);                                           //while loop
+	                }
+  }
+
+}
+      
