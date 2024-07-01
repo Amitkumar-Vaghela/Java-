66841b68618f926d4b6aae0f793f99d9d72e1735
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-01 21:34:02 +0530

    Create time_complexity.java

---
+++ time_complexity.java
@@ -0,0 +1,32 @@
+package algorithams;
+
+import java.util.Arrays;
+
+/*
+ * Consider Notes : PDF : #1_Algorithms.pdf
+ */
+public class Big_O_TimeComplexityDemo {
+
+// Constant Time Complexity (O(1)):	
+	  public static int constantTime_firstElement(int[] array) {
+	        return array[0];
+	    }
+
+	  
+// Linear Time Complexity (O(n)):	  
+	  public static int linearTime_firstElement(int[] array) {
+	        for (int i = 0; i < array.length;) {
+	            return array[0];
+	        }
+	        return -1; // This line is unreachable in this example but added for completeness
+	    }
+	  
+	    public static int linearTime_calcFactorial(int n) {
+	        int factorial = 1;
+	        for (int i = 2; i <= n; i++) {
+	            factorial *= i;
+	        }
+	        return factorial;
+	    }
+
+
