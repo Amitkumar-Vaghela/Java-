3f9c10e1962db6b4393656121a09adb5d51a1719
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-04 21:38:18 +0530

    Update time_complexity.java

--- time_complexity.java
+++ time_complexity.java
@@ -62,3 +62,67 @@ public class Big_O_TimeComplexityDemo {
 	    }
 
 
+// Exponential Time Complexity : O(2^n)	    
+	    public static int exponentialTime_recursiveFibonacci(int n) {
+	        if (n < 2) {
+	            return n;
+	        }
+	        return exponentialTime_recursiveFibonacci(n - 1) + exponentialTime_recursiveFibonacci(n - 2);
+	    }
+	    
+
+// Linearithmic Time Complexity (O(n log n)):	    
+	    
+	public static void mergeSort(int[] arr, int low, int high) {
+		    
+	    	    if (low < high) 
+         {	                                         // Find the middle point
+	    	    int mid = (low + high) / 2;
+	    	                                         // Sort first and second halves
+	    	    mergeSort(arr, low, mid);
+	    	    mergeSort(arr, mid + 1, high);
+	    	                                          // Merge the sorted halves
+	    	    merge(arr, low, mid, high);
+	    	  }
+	    	}
+	    	public static void merge(int[] arr, int low, int mid, int high) {
+	    	                                  // Find sizes of two subarrays to be merged
+	    	  int n1 = mid - low + 1;
+	    	  int n2 = high - mid;
+	    	                                 // Create temp arrays
+	    	  int[] L = new int[n1];
+	    	  int[] R = new int[n2];
+	    	                                  // Copy data to temp arrays
+	    	  for (int i = 0; i < n1; i++)
+	    	    L[i] = arr[low + i];
+	    	  for (int j = 0; j < n2; j++)
+	    	    R[j] = arr[mid + 1 + j];
+	    	                                // Merge the temp arrays
+	    	                               // Initial indexes of first and second subarrays
+	    	  int i = 0, j = 0;
+	    	                               // Initial index of merged subarray
+	    	  int k = low;
+	    	  while (i < n1 && j < n2) {
+	    	    if (L[i] <= R[j]) {
+	    	      arr[k] = L[i];
+	    	      i++;
+	    	    } else {
+	    	      arr[k] = R[j];
+	    	      j++;
+	    	    }
+	    	    k++;
+	    	  }
+	    	                            // Copy remaining elements of L[] if any
+	    	  while (i < n1) {
+	    	    arr[k] = L[i];
+	    	    i++;
+	    	    k++;
+	    	  }
+	    	                           // Copy remaining elements of R[] if any
+	    	  while (j < n2) {
+	    	    arr[k] = R[j];
+	    	    j++;
+	    	    k++;
+	    	  }
+	    	}
+
